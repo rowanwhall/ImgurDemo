be648e03e595578ad3f306c9c8aa87ef77e647fb
Rowan Hall <rhall1705@gmail.com>
2020-06-29 19:57:11 -0400

    version upgrades, convert feed detail to viewpager2

--- app/src/main/java/personal/rowan/imgur/feed/FeedDetailFragment.kt
+++ app/src/main/java/personal/rowan/imgur/feed/FeedDetailFragment.kt
@@ -7,8 +7,10 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.RecyclerView
 import personal.rowan.imgur.R
 import personal.rowan.imgur.databinding.FragmentFeedDetailBinding
+import personal.rowan.imgur.databinding.ListItemFeedDetailBinding
 
 /**
  * Created by Rowan Hall
@@ -26,7 +28,28 @@ class FeedDetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.imageUrl = args.populatedGallery.images[0].link
+        binding.detailViewpager.adapter = FeedDetailAdapter(args.populatedGallery.images.map { it.link })
     }
 
+}
+
+class FeedDetailAdapter(private val imageUrls: List<String>) : RecyclerView.Adapter<FeedDetailViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FeedDetailViewHolder {
+        return FeedDetailViewHolder(ListItemFeedDetailBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+
+    override fun getItemCount(): Int {
+        return imageUrls.size
+    }
+
+    override fun onBindViewHolder(holder: FeedDetailViewHolder, position: Int) {
+        holder.bind(imageUrls[position])
+    }
+}
+
+class FeedDetailViewHolder(private val binding: ListItemFeedDetailBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(imageUrl: String) {
+        binding.imageUrl = imageUrl
+    }
 }
